1664553e9857bc0ff962c170c811680d7ffb59c4
ahmedmoh1234 <ahmedmoh123@hotmail.com>
2022-12-19 22:05:22 +0200

    DANGER test

--- unitTesting.py
+++ unitTesting.py
@@ -1,10 +1,13 @@
 import numpy as np
+import time
 import adaboost.adaboost as adaboost
+import adaboost.haarlikefeatures as HaarLikeFeatures
 
 class UnitTest():
 
     def __init__(self, img) -> None:
         self.test_integralImage(img)
+        self.test_determineFeatures()
         return
 
     def test_integralImage(self, img) -> None:
@@ -19,3 +22,24 @@ class UnitTest():
         intImg2 = adaboost.integralImage(img)
         if intImg.all() == intImg2.all():
             print('Integral image test PASSED')
+
+
+    def test_determineFeatures(self) -> None:
+        # img : input image
+        # n : number of rows
+        # m : number of columns
+        # intImg : integral image
+        # features : list of features
+
+        #calculate integral image with first row and first column as zeros
+        #because this is a convention
+        
+        img = np.ones((384,288))
+        # print(img.shape)
+        print('Determining features...')
+        t1 = time.time()
+        hlf = HaarLikeFeatures.HaarLikeFeature(0,0,24,24,HaarLikeFeatures.HaarLikeFeature.HaarType.TWO_VERTICAL,0,1)
+        features = hlf.determineFeatures(img,0,24,24)
+        t2 = time.time()
+        print('Number of features: ',len(features))
+        print('Time taken to determine features: ',t2-t1)
