d2ebaa46ca2bfa667e6045b8bbfbca54b287bf64
ahmedmoh1234 <ahmedmoh123@hotmail.com>
2022-12-19 18:34:42 +0200

    Create unitTesting.py

---
+++ unitTesting.py
@@ -0,0 +1,21 @@
+import numpy as np
+import adaboost.adaboost as adaboost
+
+class UnitTest():
+
+    def __init__(self, img) :
+        self.test_integralImage(img)
+        return
+
+    def test_integralImage(self, img):
+        # img : input image
+        # n : number of rows
+        # m : number of columns
+        # intImg : integral image
+
+        #calculate integral image with first row and first column as zeros
+        #because this is a convention
+        intImg = np.cumsum(np.cumsum(np.pad(img,((1,0),(1,0)),'constant'),axis=0),axis=1)
+        intImg2 = adaboost.integralImage(img)
+        if intImg.all() == intImg2.all():
+            print('Integral image test PASSED')
