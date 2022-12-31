efec291eaa506ea937f09336736f3b9ae95abf54
ahmedmoh1234 <ahmedmoh123@hotmail.com>
2022-12-31 17:16:34 +0200

    Integration + cleanup

--- Detection/DetectionMain.py
+++ Detection/DetectionMain.py
@@ -5,157 +5,160 @@ from skimage.transform import resize
 import time
 import numpy as np
 import os
+import sys
 import copy
 from tqdm.auto import tqdm
 
-from unitTesting import UnitTest
+# from unitTesting import UnitTest
 import adaboost.adaboost as ab
 import utilitis as ut
-
-import os
 _file_ = os.path.abspath(__file__)
 dirname = os.path.dirname(_file_)
-test_filename = os.path.join(dirname, 'test.jpg')
-positiveDatasetPath = os.path.join(dirname, 'Dataset/posTrain2.npy')
-
-img = io.imread(test_filename)
-img = rgb2gray(img)
-unitTesting = UnitTest(img)
 
 
-# load positive dataset
-try:
-    print("Loading positive dataset...")
-    positiveDataset = np.load(positiveDatasetPath)
-except:
-    print("No dataset found, creating new dataset...")
-    # positiveDataset = np.load("./Dataset/olivetti_faces.npy")
-    positiveDataset = np.load("./testImg/postTrain.npy")
-    personID = np.load("./Dataset/olivetti_faces_target.npy")
+#===============================================Training ===============================================
+def trainDetector():
+    
+    positiveDatasetPath = os.path.join(dirname, 'Dataset/posTrain2.npy')
 
-    # resize images to 24x24
-    newPositiveDataset = np.empty(shape=(positiveDataset.shape[0], 24, 24))
-    for i in range(len(positiveDataset)):
-        newImg = np.array(positiveDataset[i])
+    # Not needed for now
+    # img = io.imread(test_filename)
+    # img = rgb2gray(img)
+    # unitTesting = UnitTest(img)
 
-        finalImg = resize(newImg, (24, 24))
-        newPositiveDataset[i] = finalImg
 
-    positiveDataset = newPositiveDataset
+    # load positive dataset
+    try:
+        # print("Loading positive dataset...")
+        positiveDataset = np.load(positiveDatasetPath)
+    except:
+        print("No dataset found, creating new dataset...")
+        # positiveDataset = np.load("./Dataset/olivetti_faces.npy")
+        positiveDataset = np.load("./testImg/postTrain.npy")
+        personID = np.load("./Dataset/olivetti_faces_target.npy")
 
-    # for i in range(10):
-    #     io.imshow(positiveDataset[i])
-    #     io.show()
+        # resize images to 24x24
+        newPositiveDataset = np.empty(shape=(positiveDataset.shape[0], 24, 24))
+        for i in range(len(positiveDataset)):
+            newImg = np.array(positiveDataset[i])
 
-    # print(positiveDataset.shape)
+            finalImg = resize(newImg, (24, 24))
+            newPositiveDataset[i] = finalImg
 
-    # np.save("./Dataset/positiveDataset.npy", positiveDataset)
-    # print("Positive Dataset saved")
+        positiveDataset = newPositiveDataset
 
-# loading negative dataset
-try:
-    print("Loading negative dataset...")
-    negativeDatasetPath = os.path.join(dirname, 'Dataset/negTrain.npy')
-    negativeDataset = np.load(negativeDatasetPath)
-except:
-    print("No dataset found, creating new dataset...")
-    directory = "./Dataset/NegativeSet"
-    negativeDataset = np.empty(shape=(len(os.listdir(directory)), 24, 24))
+        # for i in range(10):
+        #     io.imshow(positiveDataset[i])
+        #     io.show()
+
+        # print(positiveDataset.shape)
+
+        # np.save("./Dataset/positiveDataset.npy", positiveDataset)
+        # print("Positive Dataset saved")
+
+    # loading negative dataset
+    try:
+        print("Loading negative dataset...")
+        negativeDatasetPath = os.path.join(dirname, 'Dataset/negTrain.npy')
+        negativeDataset = np.load(negativeDatasetPath)
+    except:
+        print("No dataset found, creating new dataset...")
+        directory = "./Dataset/NegativeSet"
+        negativeDataset = np.empty(shape=(len(os.listdir(directory)), 24, 24))
+        i = 0
+        for filename in os.listdir(directory):
+            if filename.endswith(".jpg"):
+                # test_filename = os.path.join(dirname, 'test.jpg')
+                curImg = io.imread(os.path.join(directory, filename), as_gray=True)
+                newImg = resize(curImg, (24, 24))
+                negativeDataset[i] = newImg
+                i += 1
+        # negativeDataset = negativeDataset.reshape(len(os.listdir(directory)), 64, 64)
+        # print(negativeDataset.shape)
+        # np.save("./Dataset/negativeDataset.npy", negativeDataset)
+        # print("Negative Dataset saved")
+
+
+    posDataset, negDataset = ut.preprocessImages(positiveDataset, negativeDataset)
+
+    # print(f"Positive Dataset Shape: {posDataset.shape}")
+    # print(f"Negative Dataset Shape: {negDataset.shape}")
+
+    minFeatureWidth = 4
+    maxFeatureWidth = 20
+    minFeatureHeight = 4
+    maxFeatureHeight = 20
+    nClassifiers = 5001
+    threshold = 0
+
+    classifiersToBeUsed = []
+    try:
+        # print("Loading classifiers...")
+        classifiersToBeUsedPath = os.path.join(dirname, 'Classifiers/classifiers5001-0-4-20.npy')
+        classifiersToBeUsed = np.load(classifiersToBeUsedPath, allow_pickle=True)
+    except Exception as e:
+        # print(e)
+        print("No classifiers found, creating new classifiers...")
+        classifier = ab.AdaBoost()
+        start = time.time()
+        classifiersToBeUsed = classifier.learn(
+            posDataset, negDataset, threshold, minFeatureWidth, minFeatureHeight, maxFeatureWidth, maxFeatureHeight, nClassifiers)
+        end = time.time()
+        classifiersToBeUsed = np.array(classifiersToBeUsed)
+        np.save("./classifiers"+str(nClassifiers)+"-" + str(threshold) + "-" +
+                str(minFeatureWidth)+"-" + str(maxFeatureWidth) + ".npy", classifiersToBeUsed)
+        print("Time taken to train the classifier: ", end-start)
+
+    print("Classifiers loaded successfully")
+    return classifiersToBeUsed
+
+#===============================================Testing ===============================================
+def testDetector(classifiersToBeUsed):
+    testDataSetPath = os.path.join(dirname, 'Dataset/testMix.npy')
+    testDataSet = np.load(testDataSetPath)
+    testDataSetTargetPath = os.path.join(dirname, "Dataset/testTargets.npy")
+
+    testDataSetTarget = np.load(testDataSetTargetPath)
+    # convert to int
+    testDataSetTarget = testDataSetTarget.astype(int)
+
+    # print(f"Test Dataset Shape: {testDataSet.shape}")
+
+    # get integral images
+    ii = np.array([ut.integralImage(test) for test in testDataSet])
+
     i = 0
-    for filename in os.listdir(directory):
-        if filename.endswith(".jpg"):
-            # test_filename = os.path.join(dirname, 'test.jpg')
-            curImg = io.imread(os.path.join(directory, filename), as_gray=True)
-            newImg = resize(curImg, (24, 24))
-            negativeDataset[i] = newImg
-            i += 1
-    # negativeDataset = negativeDataset.reshape(len(os.listdir(directory)), 64, 64)
-    # print(negativeDataset.shape)
-    # np.save("./Dataset/negativeDataset.npy", negativeDataset)
-    # print("Negative Dataset saved")
-
-
-posDataset, negDataset = ut.preprocessImages(positiveDataset, negativeDataset)
-
-print(f"Positive Dataset Shape: {posDataset.shape}")
-print(f"Negative Dataset Shape: {negDataset.shape}")
-
-minFeatureWidth = 4
-maxFeatureWidth = 20
-minFeatureHeight = 4
-maxFeatureHeight = 20
-nClassifiers = 5001
-threshold = 0
-
-classifiersToBeUsed = []
-try:
-    print("Loading classifiers...")
-    classifiersToBeUsedPath = os.path.join(
-        dirname, 'classifiers5001-0-4-20.npy')
-    classifiersToBeUsed = np.load(
-        classifiersToBeUsedPath, allow_pickle=True)
-except Exception as e:
-    # print(e)
-    print("No classifiers found, creating new classifiers...")
-    classifier = ab.AdaBoost()
-    start = time.time()
-    classifiersToBeUsed = classifier.learn(
-        posDataset, negDataset, threshold, minFeatureWidth, minFeatureHeight, maxFeatureWidth, maxFeatureHeight, nClassifiers)
-    end = time.time()
-    classifiersToBeUsed = np.array(classifiersToBeUsed)
-    np.save("./classifiers"+str(nClassifiers)+"-" + str(threshold) + "-" +
-            str(minFeatureWidth)+"-" + str(maxFeatureWidth) + ".npy", classifiersToBeUsed)
-    print("Time taken to train the classifier: ", end-start)
-
-print("Classifiers loaded successfully")
-
-testDataSetPath = os.path.join(dirname, 'Dataset/testMix.npy')
-testDataSet = np.load(testDataSetPath)
-testDataSetTargetPath = os.path.join(dirname, "Dataset/testTargets.npy")
-
-testDataSetTarget = np.load(testDataSetTargetPath)
-# convert to int
-testDataSetTarget = testDataSetTarget.astype(int)
-
-print(f"Test Dataset Shape: {testDataSet.shape}")
-
-# get integral images
-ii = np.array([ut.integralImage(test) for test in testDataSet])
-
-i = 0
-correctCount = 0
-firstWrong = 5
-facesCount = 0
-correctFacesCount = 0
-correctNonFacesCount = 0
-nonFacesCount = 0
-for test in ii:
-    predicted = ut.getVotes(np.array(classifiersToBeUsed), test)
-    if predicted == testDataSetTarget[i]:
-        correctCount += 1
-        if predicted == 1:
-            correctFacesCount += 1
+    correctCount = 0
+    firstWrong = 5
+    facesCount = 0
+    correctFacesCount = 0
+    correctNonFacesCount = 0
+    nonFacesCount = 0
+    for test in ii:
+        predicted = ut.getVotes(np.array(classifiersToBeUsed), test)
+        if predicted == testDataSetTarget[i]:
+            correctCount += 1
+            if predicted == 1:
+                correctFacesCount += 1
+            else:
+                correctNonFacesCount += 1
         else:
-            correctNonFacesCount += 1
-    else:
-        if firstWrong > 0:
-            print("Predicted: ", predicted)
-            print("Target: ", testDataSetTarget[i])
-            # io.imshow(testDataSet[i])
-            # io.show()
-            firstWrong -= 1
-    if testDataSetTarget[i] == 1:
-        facesCount += 1
-    if testDataSetTarget[i] == 0:
-        nonFacesCount += 1
+            if firstWrong > 0:
+                print("Predicted: ", predicted)
+                print("Target: ", testDataSetTarget[i])
+                # io.imshow(testDataSet[i])
+                # io.show()
+                firstWrong -= 1
+        if testDataSetTarget[i] == 1:
+            facesCount += 1
+        if testDataSetTarget[i] == 0:
+            nonFacesCount += 1
 
-    i += 1
+        i += 1
 
-print("Accuracy: ", correctCount/len(testDataSet) * 100, "%")
-print(
-    f"Faces detected: {correctFacesCount} / {facesCount} , Accuracy: {correctFacesCount/facesCount * 100}%")
-print(
-    f"Non Faces detected: {correctNonFacesCount} / {nonFacesCount} , Accuracy: {correctNonFacesCount/nonFacesCount * 100}%")
+    print("Accuracy: ", correctCount/len(testDataSet) * 100, "%")
+    print(f"Faces detected: {correctFacesCount} / {facesCount} , Accuracy: {correctFacesCount/facesCount * 100}%")
+    print(f"Non Faces detected: {correctNonFacesCount} / {nonFacesCount} , Accuracy: {correctNonFacesCount/nonFacesCount * 100}%")
 
 
 def scaleFeatures(features, scale):
@@ -163,10 +166,10 @@ def scaleFeatures(features, scale):
     features2 = copy.deepcopy(features)
     try:
         finalFeaturesPath = os.path.join(
-            dirname, 'scaledFeatures'+str(scale) + '.npy')
+            dirname, 'ScaledFeatures/scaledFeatures'+str(scale) + '.npy')
         finalFeatures = np.load(
             finalFeaturesPath, allow_pickle=True)
-        print(f"Features with scale {scale} loaded successfully")
+        # print(f"Features with scale {scale} loaded successfully")
         return np.array(finalFeatures)
     except:
         print("No features found, creating new features...")
@@ -189,7 +192,8 @@ def scaleFeatures(features, scale):
 
 def predict(img2, classifiersToBeUsedFn) -> list:
     img = np.copy(img2)
-    img = rgb2gray(img)
+    if len(img.shape) == 3:
+        img = rgb2gray(img)
     img = img/img.max()
     img = img / np.var(img)
 
@@ -265,15 +269,16 @@ def predict(img2, classifiersToBeUsedFn) -> list:
     # print(f"scale factor: {scaledFeatures5[0].scale} \n")
 
     # print(f"shape of scaled features: {scaledFeatures[0].shape}")
+
+    print("Starting to detect faces...")
     shiftValue = 1
     for features in scaledFeatures:
         scaleFactor = features[0].scale
-        print(
-            f"Scale factor: {scaleFactor} and max scale factor: {maxScaleFactor}")
+        # print(f"Scale factor: {scaleFactor} and max scale factor: {maxScaleFactor}")
         if scaleFactor > maxScaleFactor:
             print("Scale factor greater than max scale factor, breaking")
             break
-        print(f"\nRunning scale factor: {scaleFactor}")
+        # print(f"\nRunning scale factor: {scaleFactor}")
         # print max x and y in features
         # maxX = 0
         # maxY = 0
@@ -302,32 +307,10 @@ def predict(img2, classifiersToBeUsedFn) -> list:
                     detectedFaces.append([x, y, scaleFactor])
                 scaledFeatures2 = [
                     sf + (0, int(np.round(shiftValue * scaleFactor))) for sf in scaledFeatures2]
-        print("Finished scale factor: ", scaleFactor)
+        # print("Finished scale factor: ", scaleFactor)
 
     return detectedFaces
 
-
-def drawRect(img2, x, y, scaleFactor):
-    img = np.copy(img2)
-    img = img/img.max()
-    if len(img.shape) == 3:
-        img = rgb2gray(img)
-    # print(img.shape)
-    imgHeight, imgWidth = img.shape
-    x = int(x)
-    y = int(y)
-    scaleFactor = int(scaleFactor)
-    for i in range(x, x + (24 * scaleFactor)):
-        if i < imgWidth and y < imgHeight:
-            img[y][i] = 1
-        if i < imgWidth and y + (24 * scaleFactor) < imgHeight:
-            img[y + (24 * scaleFactor)][i] = 1
-    for i in range(y, y + (24 * scaleFactor)):
-        if i < imgHeight and x < imgWidth:
-            img[i][x] = 1
-        if i < imgHeight and x + (24 * scaleFactor) < imgWidth:
-            img[i][x + (24 * scaleFactor)] = 1
-    return img
 
 
 def detectFaces(img, classifiersToBeUsedFn, threshold=0):
@@ -337,8 +320,6 @@ def detectFaces(img, classifiersToBeUsedFn, threshold=0):
 
     if len(detectedFaces) > threshold:
         print(f"==========Face detected==============")
-        for face in detectedFaces:
-            img = drawRect(img, face[0], face[1], face[2])
         return img
     else:
         print("================No faces detected================")
@@ -354,6 +335,7 @@ def detectFaces(img, classifiersToBeUsedFn, threshold=0):
 # detectedFace = detectFaces(img, classifiersToBeUsed, threshold)
 
 
-def detector_main(input_image):
+def detector_main(input_image, classifiersToBeUsed):
     detectedFace = detectFaces(input_image, classifiersToBeUsed, 30)
-    return detectedFace != None
+    return detectedFace is not None
+
